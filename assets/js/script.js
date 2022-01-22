0e2786223ddded4966d7090ef0c8347a470e9bbc
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-01-22 13:59:31 +0000

    changes to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,40 @@
 // TODO: Find the Search Form in the DOM and assign it to a variable
 
+var searchFormEl = document.querySelector('#search-form');
+var searchInput = document.querySelectorAll('#search-input');
+var formatInput = document.querySelectorAll('#format-input');
+var searchTerm = document.querySelector('#search-term');
+var resultContainerEl = document.querySelector('#result-content');
+var submitForm = document.querySelector('#submit-form')
+
 function handleSearchFormSubmit(event) {
   event.preventDefault();
+
+  var keyword = nameInputEl.value.trim();
+
+  if (keyword) {
+    getRecords(keyword);
+
+    resultContainerEl.textContent = '';
+    searchInput.value = '';
+
+  } else {
+    alert('Please enter a keyword');
+  }
+};
+
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute('data-language');
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    repoContainerEl.textContent = '';
+  }
+};
+
+
+
 
   // TODO: Obtain the values submitted in the form and assign them to variables
 
@@ -13,3 +46,110 @@ function handleSearchFormSubmit(event) {
 }
 
 // TODO: Add the above function as an event handler for the form submission
+
+
+
+var searchInput = document.querySelector('#search-input');
+var formatInput = document.querySelector('#format-input');
+var SearchTerm = document.querySelector('#search-term');
+var userFormEl = document.querySelector('#user-form');
+var repoContainerEl = document.querySelector('#repos-container');
+
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+
+    repoContainerEl.textContent = '';
+    nameInputEl.value = '';
+  } else {
+    alert('Please enter a GitHub username');
+  }
+};
+
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute('data-language');
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    repoContainerEl.textContent = '';
+  }
+};
+
+var getUserRepos = function (user) {
+  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayRepos(data, user);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to GitHub');
+    });
+};
+
+var getFeaturedRepos = function (language) {
+  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  });
+};
+
+var displayRepos = function (repos, searchTerm) {
+  if (repos.length === 0) {
+    repoContainerEl.textContent = 'No repositories found.';
+    return;
+  }
+
+  repoSearchTerm.textContent = searchTerm;
+
+  for (var i = 0; i < repos.length; i++) {
+    var repoName = repos[i].owner.login + '/' + repos[i].name;
+
+    var repoEl = document.createElement('a');
+    repoEl.classList = 'list-item flex-row justify-space-between align-center';
+    repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
+
+    var titleEl = document.createElement('span');
+    titleEl.textContent = repoName;
+
+    repoEl.appendChild(titleEl);
+
+    var statusEl = document.createElement('span');
+    statusEl.classList = 'flex-row align-center';
+
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    repoEl.appendChild(statusEl);
+
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+
+userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener('click', buttonClickHandler);
